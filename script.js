e3e0f80467f211ff8925e439282d067cd2d59d0e
creatorpilota <creatorpilota@gmail.com>
2026-07-22 20:18:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-const output = document.getElementById("output");
+const output = document.getElementById("output");const API_KEY = AIzaSyC7qZo2NqxO6YsHA0cLfHLLo-VxHAVzN5o
+
+async function askAI(prompt) {
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      contents: [
+        {
+          parts: [
+            {
+              text: prompt
+            }
+          ]
+        }
+      ]
+    })
+  });
+
+  const data = await response.json();
+  return data.candidates[0].content.parts[0].text;
+}
 const loading = document.getElementById("loading");
 
 function showLoading() {
